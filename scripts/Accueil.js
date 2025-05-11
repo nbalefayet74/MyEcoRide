b1f19e93a48697816b9aa3ba39b85ee1c9426079
ISHAK435 <aliamira930@gmail.com>
2025-05-11 15:42:48 +0200

    Add files via upload

---
+++ scripts/Accueil.js
@@ -0,0 +1,66 @@
+<script>
+var menuItems= document.querySelectorAll('#menu li');
+for (var je = 0; je menuItems.length; je++) menuItems[i].addventlistener('mouseover', function())
+this.classList.add('hover');
+    menuItems[i].addventlistener'mouseout',fonction()this.classList.remove('hover')
+
+    ;
+
+
+</script>
+document.getElementById('myButton').addEventListener('click', function() {
+alert('Bonjour ! Bienvenue sur mon site.');
+});
+export default class Accueil {
+
+constructor(id) {
+this.id=id;
+this.Accueil.Html = document.getElementById(this.id);
+this.Accueil = new Accueil(this.formulaireHtml);
+this.answers = new Array();
+}
+}
+
+
+export class Card {
+constructor(imageName, alt, href, title, description, className = "", id = "") {
+  this.imageName = imageName;
+  this.alt = alt;
+  this.href = href;
+  this.title = title;
+  this.description = description;
+  this.className = className;
+  this.id = id;
+}
+
+getId() {
+  return this.id;
+}
+
+getImageName() {
+  return this.imageName;
+}
+
+getAlt() {
+  return this.alt;
+}
+
+getHref() {
+  return this.href;
+}
+
+getTitle() {
+  return this.title;
+}
+
+getDescription() {
+  return this.description;
+}
+
+getClassName() {
+  return this.className;
+}
+}
+
+
+
