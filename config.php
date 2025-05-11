b5bb630b436ffbd39287d64829c6cb3715e65e9e
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 16:53:55 +0200

    Update config.php
    Signed-off-by: nbalefayet74 <nathaliebaque74@gmail.com>

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost';
+$host = '127.0.0.1';
+$port = '444';
 $db   = 'myecoride';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=$charset", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
